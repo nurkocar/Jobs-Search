d60f88a5223f4ed4759f716ac95fc76e052889e2
Nur Kocar <64444237+nurkocar@users.noreply.github.com>
2020-11-03 19:04:11 +0000

    created the pages and Router

---
+++ src/Router.js
@@ -0,0 +1,23 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import {Introduction, Jobs, SavedJobs} from './pages';
+
+const Stack = createStackNavigator();
+
+function Router() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Intro" component={Introduction} />
+        <Stack.Screen name="Jobs" component={Jobs} />
+        <Stack.Screen name="SavedJobs" component={SavedJobs} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Router;
